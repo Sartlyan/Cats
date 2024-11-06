82d9d15540293703270e4c039a9033f740f621db
Sartlyan <shadrinaspb@gmail.com>
2024-11-06 03:43:03 +0300

    Вместо кнопки добавлено меню

--- Cataas.py
+++ Cataas.py
@@ -4,6 +4,7 @@ import requests
 from io import BytesIO
 
 from bottle import response
+from gevent.testing.travis import command
 from pygame.examples.cursors import image
 
 
@@ -14,8 +15,6 @@ def load_image(url):
         image_data=BytesIO(response.content)
         img=image.open(image_data)
         img.thumbnail((600,480), Image.Resampling.LANCZOS)
-
-
         return ImageTk.PhotoImage(img)
     except Exception as e:
         print(f"Произошла ошибка: {e}")
@@ -29,7 +28,8 @@ def set_image():
         label.config(image=img)
         label.image = img
 
-
+def exit():
+    window.destroy()
 
 window=Tk()
 window.title("Cats!")
@@ -38,8 +38,17 @@ window.geometry("600x520")
 label=Label()
 label.pack()
 
-update_button=Button(text="Обновить", command=set_image)
-update_button.pack()
+# update_button=Button(text="Обновить", command=set_image)
+# update_button.pack()
+
+menu_bar=menu(window)
+window.config(menu=menu_bar)
+
+fail_menu=menu(menu_bar, tearoff=0)
+menu_bar.add_coscade(label="Файл", menu=fail_menu)
+fail_menu.add_command(label="Загрузить фото", command=set_image)
+fale_menu.add_separator()
+fail_menu.add_command(label="Выход",command=exit)
 
 url="https://cataas.com/cat"
 
