ababef35144edb33f2051911cfd0e6412dbbd20b
Sartlyan <shadrinaspb@gmail.com>
2024-11-06 11:21:55 +0300

    Теперь каждое изображение открывается в новом окне

--- Cataas.py
+++ Cataas.py
@@ -3,9 +3,7 @@ from PIL import Image, ImageTk
 import requests
 from io import BytesIO
 
-from bottle import response
-from gevent.testing.travis import command
-from pygame.examples.cursors import image
+
 
 
 def load_image(url):
@@ -13,7 +11,7 @@ def load_image(url):
         response=requests.get(url)
         response.raise_for_status()
         image_data=BytesIO(response.content)
-        img=image.open(image_data)
+        img=Image.open(image_data)
         img.thumbnail((600,480), Image.Resampling.LANCZOS)
         return ImageTk.PhotoImage(img)
     except Exception as e:
@@ -21,12 +19,16 @@ def load_image(url):
         return None
 
 
-def set_image():
+def open_new_window():
     img = load_image(url)
-
     if img:
-        label.config(image=img)
+        new_window=Toplevel()
+        new_window.title("Картинка с котиком")
+        new_window.geometry("600x480")
+
+        label=Label(new_window,image=img)
         label.image = img
+        label.pack()
 
 def exit():
     window.destroy()
@@ -35,24 +37,16 @@ window=Tk()
 window.title("Cats!")
 window.geometry("600x520")
 
-label=Label()
-label.pack()
-
-# update_button=Button(text="Обновить", command=set_image)
-# update_button.pack()
-
-menu_bar=menu(window)
+menu_bar=Menu(window)
 window.config(menu=menu_bar)
 
-fail_menu=menu(menu_bar, tearoff=0)
-menu_bar.add_coscade(label="Файл", menu=fail_menu)
-fail_menu.add_command(label="Загрузить фото", command=set_image)
-fale_menu.add_separator()
-fail_menu.add_command(label="Выход",command=exit)
+file_menu=Menu(menu_bar, tearoff=0)
+menu_bar.add_cascade(label="Файл", menu=file_menu)
+file_menu.add_command(label="Загрузить фото", command=open_new_window)
+file_menu.add_separator()
+file_menu.add_command(label="Выход",command=exit)
 
 url="https://cataas.com/cat"
-
-set_image()
 
 window.mainloop()
 
